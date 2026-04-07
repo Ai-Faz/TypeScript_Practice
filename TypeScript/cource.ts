0bc2269691653aee08cb61c1744263666e6e63c5
Aifaz <aifazmo101@gmail.com>
2026-04-07 15:33:26 +0530

    TypeScript Main File add

---
+++ TypeScript/cource.ts
@@ -0,0 +1,28 @@
+var x : number;
+x = 31;
+
+console.log(x);
+
+var flag : boolean = true;
+flag; true 
+
+var  firstname : string = "Aifaz"
+
+firstname; 
+
+
+var iamnot : any =" :)hi "
+iamnot
+
+var arr : number[] = [1,2,3,4]
+arr
+
+var persone : any = [
+    {id:1, name:'aifaz'},
+    {id:2, name:'ayaan'},
+    {id:3, name:'zahed'},
+    {id:4, name:'faheem'}
+];
+
+persone
+
