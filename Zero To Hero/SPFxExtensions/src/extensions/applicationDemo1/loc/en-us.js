0272376976091b2a1a1614a42e6de0a0db6ea9aa
Aifaz <aifazmo101@gmail.com>
2026-04-16 14:45:06 +0530

    folder manages

---
+++ Zero To Hero/SPFxExtensions/src/extensions/applicationDemo1/loc/en-us.js
@@ -0,0 +1,5 @@
+define([], function() {
+  return {
+    "Title": "ApplicationDemo1ApplicationCustomizer"
+  }
+});
---
+++ Zero To Hero/SPFxExtensions/src/extensions/applicationDemo1/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IApplicationDemo1ApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'ApplicationDemo1ApplicationCustomizerStrings' {
+  const strings: IApplicationDemo1ApplicationCustomizerStrings;
+  export = strings;
+}
