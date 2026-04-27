d00fb3cff288f89879bcbe92d665c354bfc8041a
Aifaz <aifazmo101@gmail.com>
2026-04-27 20:24:08 +0530

    add new file for dependancy

---
+++ Practice/provisionassetsdemo/src/webparts/provisionassetsexamples/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IProvisionassetsexamplesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'ProvisionassetsexamplesWebPartStrings' {
+  const strings: IProvisionassetsexamplesWebPartStrings;
+  export = strings;
+}
